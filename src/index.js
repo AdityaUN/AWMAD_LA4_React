5b606d3461f7ef2229a3a85c0a8e0ba8d1fdc2ff
Aditya Nanoskar <kwaditya395@gmail.com>
2021-06-14 15:21:11 +0530

    first

--- src/index.js
+++ src/index.js
@@ -4,12 +4,33 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class MyForm extends React.Component {
+  render() {
+    return (
+      <form>
+        <h1>Feedback Form</h1>
+        <p>Name:</p>
+        <input
+          type="text"
+        />
+        <p>Email:</p>
+        <input
+          type="email"
+        />
+        <p>Feedback:</p>
+        <input
+          type="textarea"
+        />
+        <p></p>
+        <input
+        type='submit'
+        />
+      </form>
+    );
+  }
+}
+
+ReactDOM.render(<MyForm />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
